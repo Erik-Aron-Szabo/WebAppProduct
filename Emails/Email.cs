8dd513d4cf1915dbdee53a4354c939a2baff5db2
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-04-13 17:49:03 +0200

    Version 1 (pictures doesn't load)

---
+++ Emails/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugSparyWebApp.Emails
+{
+    //email of the subscriber
+    public class Email
+    {
+        public string E_Mail { get; set; }
+        public Email(string e_Mail)
+        {
+            E_Mail = e_Mail;
+        }
+    }
+}
